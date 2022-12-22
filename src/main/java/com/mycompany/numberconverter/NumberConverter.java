27f2eeaf186eec40d09de518768a7bf5d8bb8cb1
nka2001 <89935053+nka2001@users.noreply.github.com>
2022-12-22 15:29:54 -0500

    Number Converter - Initial Commit

---
+++ src/main/java/com/mycompany/numberconverter/NumberConverter.java
@@ -0,0 +1,12 @@
+package com.mycompany.numberconverter;
+
+/**
+ * This application will be used to convert numbers to different types, such as convert decimal to binary, or decimal to hexadecimal and so on
+ * @author nicka
+ */
+public class NumberConverter {
+
+    public static void main(String[] args) {
+        
+    }
+}
